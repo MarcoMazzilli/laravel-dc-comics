1388b6992990bbb7a2dae72c0cbbb4c1ce48fa42
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-06-13 17:51:36 +0200

    [Add] Custom error message under input

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -25,47 +25,74 @@
             <datalist id="title_list">
                 <option value="titolo test">Test</option>
             </datalist>
+            @error ('title')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Path immagine </label>
             <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Path immagine">
+            @error ('thumb')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo </label>
             <input type="text" class="form-control" id="price" name="price" placeholder="Prezzo">
+            @error ('price')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label">Serie </label>
             <input type="text" class="form-control" id="series" name="series" placeholder="Serie">
+            @error ('series')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita </label>
             <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="YYYY-MM-DD">
+            @error ('sale_date')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label">Categoria </label>
             <input type="text" class="form-control" id="type" name="type" placeholder="Categoria">
+            @error ('type')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="artists" class="form-label">Artisti </label>
             <input type="text" class="form-control" id="artists" name="artists" placeholder="Artista">
+            @error ('artists')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="writers" class="form-label">Scrittori </label>
             <input type="text" class="form-control" id="writers" name="writers" placeholder="Scrittori">
+            @error ('writers')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione </label>
             <textarea class="form-control" name="description" id="description" cols="30" rows="2" placeholder="Inserisci una descrizione"></textarea>
+            @error ('description')
+                <p class="text-danger">{{ $message}}</p>
+            @enderror
         </div>
 
         <div class="text-end my-2">
